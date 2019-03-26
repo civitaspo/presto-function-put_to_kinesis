42aa35e55a7c7f47251182872c4d739e346de4f7
Civitaspo <civitaspo@gmail.com>
2019-03-26 11:43:44 +0900

    Implement the Plugin class by Java because Scala codes cannot fulfill the Presto UDF requirements.

--- src/main/scala/pro/civitaspo/presto/udf/put_to_kinesis/HelloFunction.scala
+++ src/main/scala/pro/civitaspo/presto/udf/put_to_kinesis/HelloFunction.scala
@@ -1,18 +1,15 @@
 package pro.civitaspo.presto.udf.put_to_kinesis
 
 
-import com.facebook.presto.spi.function.Description
-import com.facebook.presto.spi.function.ScalarFunction
-import com.facebook.presto.spi.function.SqlNullable
-import com.facebook.presto.spi.function.SqlType
-import io.airlift.slice.Slice
-import io.airlift.slice.Slices
 import com.facebook.presto.spi.`type`.StandardTypes.VARCHAR
+import com.facebook.presto.spi.function.{Description, ScalarFunction, SqlNullable, SqlType}
+import io.airlift.slice.{Slice, Slices}
 
 object HelloFunction {
   @ScalarFunction("hello")
   @Description("Hello Function")
-  @SqlType(VARCHAR) def hello(@SqlNullable @SqlType(VARCHAR) v: Slice): Slice = {
+  @SqlType(VARCHAR)
+  def hello(@SqlNullable @SqlType(VARCHAR) v: Slice): Slice = {
     if (v == null || v.toStringUtf8.isEmpty) Slices.utf8Slice("Hello World")
     else Slices.utf8Slice(String.format("Hello %s", v.toStringUtf8))
   }
